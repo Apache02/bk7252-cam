04ad692947bb10ac7c7256c756e130fcfbbf07cf
Aleksey Priymachuk <gxad02@gmail.com>
2024-09-28 21:35:44 +0300

    refactor: added utils module

--- src/applications/blink/blink.cpp
+++ src/applications/blink/blink.cpp
@@ -1,28 +1,8 @@
 #include <stdio.h>
 #include "drivers/uart.h"
 #include "drivers/gpio.h"
+#include "utils/sleep.h"
 
-
-static inline void busy_wait_at_least_cycles(uint32_t minimum_cycles) {
-    __asm volatile(
-    ".syntax unified\n"
-    "1: subs %0, #3\n"
-    "bcs 1b\n"
-    : "+l" (minimum_cycles) : : "cc", "memory"
-    );
-}
-
-void usleep(unsigned int us) {
-    busy_wait_at_least_cycles(us * (180'000'000 / 1'000'000));
-}
-
-void msleep(unsigned int ms) {
-    usleep(ms * 1000);
-}
-
-void sleep(unsigned int sec) {
-    usleep(sec * 1000000);
-}
 
 #define LED_PIN         26
 #define KEY_PWR_PIN     2       // pull up
--- src/applications/hello/main.cpp
+++ src/applications/hello/main.cpp
@@ -1,27 +1,8 @@
 #include <stdio.h>
 #include "drivers/uart.h"
 #include "drivers/wdt.h"
+#include "utils/sleep.h"
 
-static inline void busy_wait_at_least_cycles(uint32_t minimum_cycles) {
-    __asm volatile(
-    ".syntax unified\n"
-    "1: subs %0, #3\n"
-    "bcs 1b\n"
-    : "+l" (minimum_cycles) : : "cc", "memory"
-    );
-}
-
-void usleep(unsigned int us) {
-    busy_wait_at_least_cycles(us * (180'000'000 / 1'000'000));
-}
-
-void msleep(unsigned int ms) {
-    usleep(ms * 1000);
-}
-
-void sleep(unsigned int sec) {
-    usleep(sec * 1000000);
-}
 
 void wdt_reboot(void) {
     wdt_set(10);
