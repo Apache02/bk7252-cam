74f577f928555c62989f3ea04be330cad57aa3f6
Aleksey Priymachuk <gxad02@gmail.com>
2024-10-15 23:34:45 +0300

    refactor: build optimization

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -7,13 +7,13 @@ set(TOOLCHAIN arm-none-eabi)
 # Compilers
 set(CMAKE_C_COMPILER ${TOOLCHAIN}-gcc)
 set(CMAKE_CXX_COMPILER ${TOOLCHAIN}-g++)
-set(CMAKE_ASM_COMPILER ${TOOLCHAIN}-as)
+set(CMAKE_ASM_COMPILER ${TOOLCHAIN}-gcc)
 
 set(CPU_OPTIONS "-mcpu=arm968e-s -mthumb-interwork")
 
 set(CMAKE_C_FLAGS "${CPU_OPTIONS} -mthumb -O2 --specs=nosys.specs -fno-exceptions")
 set(CMAKE_CXX_FLAGS "${CPU_OPTIONS} -mthumb -O2 --specs=nosys.specs -fno-exceptions") #  -lstdc++ ??
-set(CMAKE_ASM_FLAGS "${CPU_OPTIONS}")
+set(CMAKE_ASM_FLAGS "${CPU_OPTIONS} -x assembler-with-cpp")
 
 #set(CMAKE_CROSSCOMPILING TRUE)
 
