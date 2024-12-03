d93e09dc9d64523cec8b9e05cbeafde030ab1bef
Aleksey Priymachuk <gxad02@gmail.com>
2024-12-03 22:31:46 +0200

    feat: memory manager is broken

--- src/platform/freertos/crt.cpp
+++ src/platform/freertos/crt.cpp
@@ -1,19 +1,29 @@
 #include <cstdlib>
 #include "FreeRTOS.h"
 
-void *operator new(size_t size) noexcept {
-    return pvPortMalloc(size);
-}
+// this code brakes console history and cases reboots
 
-void operator delete(void *ptr) noexcept {
-    vPortFree(ptr);
-}
+//void *operator new(size_t size) noexcept {
+//    return pvPortMalloc(size);
+//}
+//
+//void operator delete(void *ptr) noexcept {
+//    vPortFree(ptr);
+//}
+//
+//void *operator new[](size_t size) noexcept {
+//    return pvPortMalloc(size);
+//}
+//
+//void operator delete[](void *ptr) noexcept {
+//    vPortFree(ptr);
+//}
 
-void *operator new[](size_t size) noexcept {
-    return pvPortMalloc(size);
-}
-
-void operator delete[](void *ptr) noexcept {
-    vPortFree(ptr);
-}
+//extern "C" void *malloc(size_t size) {
+//    return pvPortMalloc(size);
+//}
+//
+//extern "C" void free(void *ptr) {
+//    vPortFree(ptr);
+//}
 
