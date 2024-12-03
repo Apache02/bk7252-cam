cd43696058da7f1c674afc2e9d00c05de75e9405
Aleksey Priymachuk <gxad02@gmail.com>
2024-12-03 23:37:12 +0200

    feat: blink task updated and fixed

--- src/applications/freertos_shell/commands/task_blink.cpp
+++ src/applications/freertos_shell/commands/task_blink.cpp
@@ -7,9 +7,10 @@
 
 #define LED_PIN         26
 #define count_of(x)     (sizeof(x) / sizeof(x[0]))
+#define TASK_NAME       "blink"
 
 
-static TaskHandle_t pxTaskBlink;
+static TaskHandle_t pxTaskBlink = NULL;
 static bool run = false;
 
 static void vTaskBlink(__unused void *pvParams) {
@@ -29,7 +30,7 @@ void command_blink(Console &c) {
     if (strcmp(pAction, "start") == 0) {
         auto result = xTaskCreate(
                 vTaskBlink,
-                "blink",
+                TASK_NAME,
                 configMINIMAL_STACK_SIZE,
                 NULL,
                 configMAX_PRIORITIES - 2,
@@ -43,14 +44,53 @@ void command_blink(Console &c) {
             printf(COLOR_RED("Task not created") "\r\n");
         }
 
-    } else if (strcmp(pAction, "stop") == 0) {
-        vTaskDelete(pxTaskBlink);
-        printf("done\r\n");
-        run = false;
-
-    } else {
-        printf(COLOR_RED("Action is not defined") "\r\n");
-        printf("Usage: %s <%s|%s>\r\n", "blink", "start", "stop");
         return;
     }
+
+    if (strcmp(pAction, "stop") == 0) {
+        if (pxTaskBlink) {
+            vTaskDelete(pxTaskBlink);
+            printf("done\r\n");
+            run = false;
+            pxTaskBlink = NULL;
+        } else {
+            printf(COLOR_RED("task not found\r\n"));
+        }
+
+        return;
+    }
+
+    if (strcmp(pAction, "stop all") == 0) {
+        uint32_t uxArrayLength = uxTaskGetNumberOfTasks();
+        size_t xTasksBufferSize = (sizeof(TaskStatus_t) * uxArrayLength);
+        TaskStatus_t *pxTasksBuffer = static_cast<TaskStatus_t *>(pvPortMalloc(xTasksBufferSize));
+
+        if (!pxTasksBuffer) {
+            printf(COLOR_RED("Can't allocate %d bytes") "\r\n", xTasksBufferSize);
+            return;
+        }
+
+        size_t uxDeletedCount = 0;
+        uxArrayLength = uxTaskGetSystemState(pxTasksBuffer, uxArrayLength, NULL);
+        if (uxArrayLength > 0) {
+            for (int i=0; i < uxArrayLength; i++) {
+                if (strcmp(pxTasksBuffer[i].pcTaskName, TASK_NAME) !=  0) continue;
+                vTaskDelete(pxTasksBuffer[i].xHandle);
+                uxDeletedCount++;
+            }
+        }
+        vPortFree(pxTasksBuffer);
+
+        if (uxDeletedCount > 0) {
+            printf("%d tasks stopped\r\n", uxDeletedCount);
+        } else {
+            printf("required tasks not found\r\n");
+        }
+
+        return;
+    }
+
+    printf(COLOR_RED("Action is not defined") "\r\n");
+    printf("Usage: %s <%s|%s|%s>\r\n", "blink", "start", "stop", "stop all");
+    return;
 }
