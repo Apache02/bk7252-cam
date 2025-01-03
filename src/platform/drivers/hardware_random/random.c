1c07a2f7abec73f5a1d1f6f956d1f5ca4983a810
Aleksey Priymachuk <gxad02@gmail.com>
2025-01-03 22:44:37 +0200

    fix: added entropy for random on enable

--- src/platform/drivers/hardware_random/random.c
+++ src/platform/drivers/hardware_random/random.c
@@ -1,13 +1,15 @@
 #include "hardware/random.h"
 #include "random_regs.h"
+#include "utils/busy_wait.h"
 
 void trng_enable() {
     hw_trng->ctrl.enable = 1;
-    get_random();
+
+    busy_wait_us(32); // time to accumulate entropy?
 }
 
 void trng_disable() {
-    hw_trng->ctrl.enable = 1;
+    hw_trng->ctrl.enable = 0;
     hw_trng->data = 0x1234;
 }
 
