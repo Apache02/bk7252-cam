b19d195088047291878e0c56490d1c08cd3fd196
Aleksey Priymachuk <gxad02@gmail.com>
2026-03-20 01:44:18 +0200

    feat: watchdog to prevent stucks

--- src/applications/freertos_shell/main.cpp
+++ src/applications/freertos_shell/main.cpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include "platform/stdio.h"
 #include "utils/panic.h"
+#include "hardware/wdt.h"
 
 
 #define count_of(x)     (sizeof(x) / sizeof(x[0]))
@@ -11,27 +12,52 @@
 
 /*-----------------------------------------------------------*/
 
-void vTaskShell(__unused void *pvParams);
+extern void vTaskShell(void *pvParams);
 
 static StaticTask_t shellTaskTCB;
 static StackType_t shellTaskStack[configMINIMAL_STACK_SIZE * 6];
 
 /*-----------------------------------------------------------*/
+
+static StaticTask_t wdtTaskTCB;
+static StackType_t wdtTaskStack[configMINIMAL_STACK_SIZE];
+
+void vTaskWdt(__unused void *pvParams) {
+    wdt_init();
+    wdt_set(2000);
+    wdt_up();
+
+    for (;;) {
+        wdt_ping();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
 /*-----------------------------------------------------------*/
 
 
-int main(void) {
+int main() {
     platform_stdio_init();
     setvbuf(stdout, NULL, _IONBF, 0);
 
     xTaskCreateStatic(
-            vTaskShell,
-            "shell",
-            count_of(shellTaskStack),
-            NULL,
-            configMAX_PRIORITIES - 1,
-            shellTaskStack,
-            &(shellTaskTCB)
+        vTaskShell,
+        "shell",
+        count_of(shellTaskStack),
+        NULL,
+        configMAX_PRIORITIES - 1,
+        shellTaskStack,
+        &(shellTaskTCB)
+    );
+
+    xTaskCreateStatic(
+        vTaskWdt,
+        "watchdog",
+        count_of(wdtTaskStack),
+        NULL,
+        configMAX_PRIORITIES - 1,
+        wdtTaskStack,
+        &(wdtTaskTCB)
     );
 
     vPortHeapResetState();
