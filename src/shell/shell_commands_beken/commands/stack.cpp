31e13484ef984e67d79ec07a8911dfcde98c2b10
Aleksey Priymachuk <gxad02@gmail.com>
2026-03-29 01:33:42 +0200

    feat: command_stack uses new Table

--- src/shell/shell_commands_beken/commands/stack.cpp
+++ src/shell/shell_commands_beken/commands/stack.cpp
@@ -1,48 +1,69 @@
 #include "shell/commands_beken.h"
 #include <stdio.h>
 #include <stdint.h>
+#include <time.h>
+#include "shell/Table.h"
 
-extern "C" uint8_t *_stack_unused;
-extern "C" uint8_t *_stack_svc;
-extern "C" uint8_t *_stack_irq;
-extern "C" uint8_t *_stack_fiq;
-extern "C" uint8_t *_stack_sys;
 
-static const uint8_t *ram_end = reinterpret_cast<uint8_t *>(0x00440000);
+#define count_of(a)  (sizeof(a) / sizeof((a)[0]))
 
-static void print_stack_region(const char *name, uint32_t color, const void *addr_start, size_t size) {
-    int count = size / sizeof(uint32_t);
-    uint32_t *ptr = reinterpret_cast<uint32_t *>(reinterpret_cast<uint32_t>(addr_start) + size);
-    int used = 0;
-    while (ptr[-1 - used] != color && (used < count)) used++;
+extern "C" uint8_t _stack_unused;
+extern "C" uint8_t _stack_svc;
+extern "C" uint8_t _stack_irq;
+extern "C" uint8_t _stack_fiq;
+extern "C" uint8_t _stack_sys;
 
-    char size_s[16];
-    sprintf(size_s, "0x%lx", static_cast<uint32_t>(size));
-    char used_s[16];
-    sprintf(used_s, "0x%x (%d%%)", used * sizeof(uint32_t), static_cast<int>(used * sizeof(uint32_t) * 100 / size));
+static const uint8_t *const ram_end = reinterpret_cast<uint8_t *>(0x00440000);
 
-    printf(
-        "| %10s | 0x%8p | %8s | 0x%08lx | %12s |\r\n",
-        name, addr_start, size_s, color, used_s
-    );
-}
+static const struct {
+    const char *name;
+    uint32_t color;
+    const uint8_t *start;
+    const uint8_t *end;
+} stack_regions[] = {
+    {"unused", 0xaaaaaaaa, &_stack_unused, &_stack_svc},
+    {"svc", 0xbbbbbbbb, &_stack_svc, &_stack_irq},
+    {"irq", 0xcccccccc, &_stack_irq, &_stack_fiq},
+    {"fiq", 0xdddddddd, &_stack_fiq, &_stack_sys},
+    {"sys", 0xeeeeeeee, &_stack_sys, ram_end},
+};
 
-#define block_size(ptr_start, ptr_end)      reinterpret_cast<unsigned int>(ptr_end) - reinterpret_cast<unsigned int>(ptr_start)
+static const Table::ColumnDef table_def[] = {
+    {"name", 10, "%s", Table::Align::Left},
+    {"color", 10, "0x%08lx", Table::Align::Right},
+    {"start", 10, "0x%08lx", Table::Align::Right},
+    {"end", 10, "0x%08lx", Table::Align::Right},
+    {"size", 8, "0x%lx", Table::Align::Right},
+    {"used", 8, "0x%lx", Table::Align::Right},
+    {"p", 4, "%3d%%", Table::Align::Right},
+};
+
 
 int command_stack(__unused int argc, __unused const char *argv[]) {
-    printf(
-        "| %10s | %10s | %8s | %10s | %12s |\r\n",
-        "name", "start", "size", "color", "used"
-    );
-    printf(
-        "| %10s | %10s | %8s | %10s | %12s |\r\n",
-        "----------", "----------", "--------", "----------", "------------"
-    );
-    print_stack_region("unused", 0xaaaaaaaa, &_stack_unused, block_size(&_stack_svc, &_stack_unused));
-    print_stack_region("svc", 0xbbbbbbbb, &_stack_svc, block_size(&_stack_irq, &_stack_svc));
-    print_stack_region("irq", 0xcccccccc, &_stack_irq, block_size(&_stack_fiq, &_stack_irq));
-    print_stack_region("fiq", 0xdddddddd, &_stack_fiq, block_size(&_stack_sys, &_stack_fiq));
-    print_stack_region("sys", 0xeeeeeeee, &_stack_sys, block_size(ram_end, &_stack_sys));
+    auto *table = new Table(table_def, count_of(table_def));
+
+    table->printHeader();
+
+    for (const auto &r: stack_regions) {
+        size_t size = r.end - r.start;
+        int count = size / sizeof(uint32_t);
+        uint32_t *top = reinterpret_cast<uint32_t *>(const_cast<uint8_t *>(r.end));
+        int used = 0;
+        while ((used < count) && top[-1 - used] != r.color) used++;
+
+        auto *row = table->createRow();
+        row->set("name", r.name);
+        row->set("color", r.color);
+        row->set("start", reinterpret_cast<uint32_t>(r.start));
+        row->set("end", reinterpret_cast<uint32_t>(r.end - 1));
+        row->set("size", static_cast<uint32_t>(size));
+        row->set("used", used * sizeof(uint32_t));
+        row->set("p", static_cast<int>(used * sizeof(uint32_t) * 100 / size));
+        table->printRow(row);
+        delete row;
+    }
+
+    delete table;
 
     return 0;
 }
