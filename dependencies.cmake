ea77d5729746d3ea901657c7abd5e353a27b2b83
Aleksey Priymachuk <gxad02@gmail.com>
2026-03-31 00:46:00 +0300

    feat: lwip downloaded by cmake

--- dependencies.cmake
+++ dependencies.cmake
@@ -15,3 +15,24 @@ if (NOT freertos_kernel_POPULATED)
 endif ()
 
 set(FREERTOS_KERNEL_PATH ${freertos_kernel_SOURCE_DIR} CACHE PATH "")
+
+
+# lwip
+message("Fetching lwip ...")
+FetchContent_Declare(
+        lwip
+        GIT_REPOSITORY  https://github.com/lwip-tcpip/lwip.git
+        GIT_TAG         STABLE-2_2_0_RELEASE
+        GIT_SHALLOW     TRUE
+)
+FetchContent_GetProperties(lwip)
+if(NOT lwip_POPULATED)
+    FetchContent_Populate(lwip)
+endif()
+
+set(LWIP_DIR ${lwip_SOURCE_DIR} CACHE PATH "")
+set(LWIP_INCLUDE_DIRS
+        "${LWIP_DIR}/src/include"
+        "${LWIP_DIR}/contrib/ports/freertos/include"
+)
+
