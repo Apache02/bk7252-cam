f2cb44982c51325d837eb9ae0161cc8e532a3804
Aleksey Priymachuk <gxad02@gmail.com>
2026-04-07 00:12:49 +0300

    fix: port_newlib POSIX errors

--- src/platform/port_newlib/syscalls.c
+++ src/platform/port_newlib/syscalls.c
@@ -1,25 +1,35 @@
+#include <sys/errno.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
-void _exit(int code) { while (1); }
 
-int _kill(int pid, int signal) { return -1; }
-
-int _close(int file) { return 0; }
-
-int _fstat(int __fd, struct stat *__sbuf) {
-    return 0;
+__attribute__((weak)) void _exit(int code) {
+    while (1);
 }
 
-int _isatty(int fd) {
-    return fd == 0 || fd == 1 || fd == 2;
-}
-
-off_t _lseek(int fd, off_t ptr, int dir) {
+__attribute__((weak)) int _kill(int pid, int signal) {
+    errno = ESRCH;
     return -1;
 }
 
-pid_t _getpid(void) {
+__attribute__((weak)) int _close(int file) {
+    return 0;
+}
+
+__attribute__((weak)) int _fstat(int __fd, struct stat *__sbuf) {
+    return 0;
+}
+
+__attribute__((weak)) int _isatty(int fd) {
+    return fd == 0 || fd == 1 || fd == 2;
+}
+
+__attribute__((weak)) off_t _lseek(int fd, off_t ptr, int dir) {
+    errno = ESPIPE;
+    return -1;
+}
+
+__attribute__((weak)) pid_t _getpid(void) {
     return 1;
 }
 
@@ -28,5 +38,6 @@ __attribute__((weak)) int _write(int file, char *ptr, int len) {
 }
 
 __attribute__((weak)) int _read(int file, char *ptr, int len) {
+    errno = EBADF;
     return -1;
 }
