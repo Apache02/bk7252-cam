5f4d2e67e3cdf834ede70a0286de58e9d44983f6
Aleksey Priymachuk <gxad02@gmail.com>
2026-04-21 00:44:52 +0300

    refactor: updated console_debug.h

--- src/shell/shell_main/include/shell/console_debug.h
+++ src/shell/shell_main/include/shell/console_debug.h
@@ -1,21 +1,31 @@
 #ifndef UTILS_SHELL_DEBUG_H
 #define UTILS_SHELL_DEBUG_H
 
+#include <stdint.h>
+#include <stdio.h>
+
 #include "console_colors.h"
 
 #ifdef DEBUG_NAME
 
-#define LOG_D(fmt, ...)      printf(COLOR_WHITE(    "D/" DEBUG_NAME " | " fmt) "\r\n", ##__VA_ARGS__)
-#define LOG_I(fmt, ...)      printf(COLOR_CYAN(     "I/" DEBUG_NAME " | " fmt) "\r\n", ##__VA_ARGS__)
-#define LOG_W(fmt, ...)      printf(COLOR_YELLOW(   "W/" DEBUG_NAME " | " fmt) "\r\n", ##__VA_ARGS__)
-#define LOG_E(fmt, ...)      printf(COLOR_RED(      "E/" DEBUG_NAME " | " fmt) "\r\n", ##__VA_ARGS__)
+#define LOG_PREFIX(level)    level "/" DEBUG_NAME " | "
+
+#define LOG_D(fmt, ...)      printf(COLOR_WHITE (LOG_PREFIX("D") fmt) "\r\n", ##__VA_ARGS__)
+#define LOG_I(fmt, ...)      printf(COLOR_CYAN  (LOG_PREFIX("I") fmt) "\r\n", ##__VA_ARGS__)
+#define LOG_W(fmt, ...)      printf(COLOR_YELLOW(LOG_PREFIX("W") fmt) "\r\n", ##__VA_ARGS__)
+#define LOG_E(fmt, ...)      printf(COLOR_RED   (LOG_PREFIX("E") fmt) "\r\n", ##__VA_ARGS__)
+
+#define LOG_REG(x)           LOG_D("%-32s @ %p = 0x%08lx", #x,        \
+                                   (const void *) &(x),               \
+                                   (unsigned long) (uint32_t) (x))
 
 #else
 
-#define LOG_I(fmt, ...)
-#define LOG_D(fmt, ...)
-#define LOG_W(fmt, ...)
-#define LOG_E(fmt, ...)
+#define LOG_D(fmt, ...)      ((void) 0)
+#define LOG_I(fmt, ...)      ((void) 0)
+#define LOG_W(fmt, ...)      ((void) 0)
+#define LOG_E(fmt, ...)      ((void) 0)
+#define LOG_REG(x)           ((void) 0)
 
 #endif
 
